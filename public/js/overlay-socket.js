b0594957d3afb54c1518fcedb7d89ff267734626
Chayut Koyvanit <entronica>
2026-06-17 23:09:08 +0700

    Add background style picker for chat and top-likers overlays

--- public/js/overlay-socket.js
+++ public/js/overlay-socket.js
@@ -2,3 +2,8 @@ function createOverlaySocket() {
   const user = new URLSearchParams(window.location.search).get('user') || '';
   return io({ query: { user } });
 }
+
+function getOverlayBgId() {
+  const bg = new URLSearchParams(window.location.search).get('bg');
+  return bg === 'transparent' ? 'transparent' : 'dim';
+}
